1fb78c1f85bfecb3ca943ee597b9243848ea837d
Shwetha Ramesh <shwetha.ramesh@moolya.com>
2023-07-20 08:02:21 +0000

    System generated commit, initial commit

---
+++ pages/Page3/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,18 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		Audio1.autoPlay
+		Tabs1.isVisible
+		MultiTreeSelect1.isDisabled
+		ButtonGroup1.isVisible
+		Checkbox1.isChecked
+		Tabs1.isVisible
+		Table1.selectedRow
+		Table2.selectedRow
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
